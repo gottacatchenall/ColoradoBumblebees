2e29fd34691c0b88cd397023e2f77338f1b0146c
michael <michael@pop-os.localdomain>
2023-03-10 10:27:37 -0500

    add helpers for chelsa download

--- scripts/make_sdms/download_chelsa.jl
+++ scripts/make_sdms/download_chelsa.jl
@@ -6,12 +6,18 @@ using Main.ColoradoBumblebees
 
 using SpeciesDistributionToolkit
 
-for s in scenarios()
+_scenario_to_projection(scenario, model=GFDL_ESM4) = Projection(scenario.ssp, model)
+function _scenario_to_year_pair(scenario)
+    return scenario.years.startyear => scenario.years.endyear
+end
+
+
+for s in scenarios()[2:end]
     tmp = [
         SimpleSDMPredictor(
             RasterData(CHELSA2, BioClim),
-            _scenario_to_projection(scenario);
-            timespan=_scenario_to_year_pair(scenario),
+            _scenario_to_projection(s);
+            timespan=_scenario_to_year_pair(s),
             layer=l,
             EXTENT...,
         ) for l in BIOLAYERS
