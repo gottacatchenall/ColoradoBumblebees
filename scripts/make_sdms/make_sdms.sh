d8d5ad77a659630c42e1a2df388166393be1e9cc
michael <michael@pop-os.localdomain>
2023-03-10 15:25:06 -0500

    :tada: cluster tests

---
+++ scripts/make_sdms/make_sdms.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+#SBATCH --time=1:00:00
+#SBATCH --account=def-gonzalez
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=32G
+
+export JULIA_DEPOT_PATH="/project/def-gonzalez/mcatchen/JuliaEnvironments/COBees"
+
+module load julia/1.8.5
+echo "Launching script..."
+julia make_sdms.jl
+#julia -t 64 make_sdms.jl
