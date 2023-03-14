2d0cb6af10073527091c1299351d4843bf49cde5
michael <michael@pop-os.localdomain>
2023-03-14 14:21:50 -0400

    :bug:

--- src/sdms/sdms.jl
+++ src/sdms/sdms.jl
@@ -236,6 +236,9 @@ end
 function compute_fit_stats_and_cutoff(distribution, coords, y)
     cutoff = LinRange(extrema(distribution)..., 500)
 
+    filter!(isnothing, coords)
+    coords = convert(Vector{typeof(coords[begin])}, coords)
+    
     obs = y .> 0
 
     tp = zeros(Float64, length(cutoff))
