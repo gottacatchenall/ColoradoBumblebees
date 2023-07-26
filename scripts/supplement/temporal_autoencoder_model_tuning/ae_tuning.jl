df10e1d03ff0eea581b912e97086803993521021
michael <michael@pop-os.localdomain>
2023-07-26 12:39:36 -0400

    ok

--- scripts/supplement/temporal_autoencoder_model_tuning/ae_tuning.jl
+++ scripts/supplement/temporal_autoencoder_model_tuning/ae_tuning.jl
@@ -110,6 +110,8 @@ end
 function main()
     cuda = true
     rnn, enc = train(LSTM, [1, 8, 1], [147,  16], [16, 147]; cuda=cuda)
+    filename = "LSTM(1,8,1)_ENC(147,16)_DEC(16,147)"
+
     write_embeddings(rnn,enc,filename; cuda=cuda)
 end
 
