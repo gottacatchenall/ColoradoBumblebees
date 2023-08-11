8e58100bbfc1156e6061032c72930b1b70eb3362
michael <mdcatchen@gmail.com>
2023-08-11 14:20:05 -0400

    :bug:

--- scripts/supplement/srl_tuning/test_distributed_classifier_fit.jl
+++ scripts/supplement/srl_tuning/test_distributed_classifier_fit.jl
@@ -9,12 +9,15 @@ function run_model(; num_replicates = 64)
     job_id = parse(Int, ENV["SLURM_ARRAY_TASK_ID"])
 
     jp = joinpath(rep_dir, temporal_reps[job_id])
-    this_embed = ColoradoBumblebees.load(jp)
+    this_rep = ColoradoBumblebees.load(jp)
 
     data = load_data()
+    feat_df = feature_dataframe(data, this_rep)
+
     model = RandomForest()
-    feat_df = feature_dataframe(data, this_embed)
-    bf = batch_fit(model, this_embed, feat_df, num_replicates)
+
+
+    bf = batch_fit(model, this_rep, feat_df, num_replicates)
     ColoradoBumblebees.save(bf)
 end
 
