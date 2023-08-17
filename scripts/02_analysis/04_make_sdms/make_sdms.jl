33f8af81ddcc9de2d67d47c1560491a6ceeae672
michael <mdcatchen@gmail.com>
2023-08-17 12:34:31 -0400

    multi job to artifacts

--- scripts/02_analysis/04_make_sdms/make_sdms.jl
+++ scripts/02_analysis/04_make_sdms/make_sdms.jl
@@ -7,23 +7,16 @@ function main()
     species = sort(unique(occurrence_df.species))
     cluster = ColoradoBumblebees.CLUSTER
 
-    for s in species 
-        sdms = make_sdms(s, occurrence_df; cluster=cluster)
-    
-        for (i,sdm) in enumerate(sdms)
-            sdm_dir = sdmdir(sdm)
-            mkpath(sdm_dir)
-            @info sdm_dir
-            SpeciesDistributionToolkit.save(joinpath(sdm_dir, "prediction.tif"), sdm.probability)
-            SpeciesDistributionToolkit.save(joinpath(sdm_dir, "uncertainty.tif"), sdm.uncertainty)
-            json_string = JSON.json(sdm.fit_stats)
-            open(joinpath(sdm_dir, "fit.json"), "w") do f
-                JSON.print(f, json_string)
-            end
-            #SpeciesDistributionToolkit.save("$(s)_tmp$i.tif", sdm.probability)
-            #ColoradoBumblebees.save(sdm)
-        end
-    end 
+    job_id = parse(Int, ENV["SLURM_ARRAY_TASK_ID"])
+    this_species = job_id
+    @info "Job: $job_id, Species: $(this_species)"
+
+    sdms = make_sdms(this_species, occurrence_df; cluster=cluster)
+    for (i,sdm) in enumerate(sdms)
+        sdm_dir = sdmdir(sdm)
+        mkpath(sdm_dir)
+        ColoradoBumblebees.save(sdm)
+    end
 
 end
 
