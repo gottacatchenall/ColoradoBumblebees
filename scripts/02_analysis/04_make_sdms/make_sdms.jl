dd1b48d50d1945fd321e599ddd23d50ad8b67d5e
michael <mdcatchen@gmail.com>
2023-08-17 11:04:30 -0400

    :bug:

--- scripts/02_analysis/04_make_sdms/make_sdms.jl
+++ scripts/02_analysis/04_make_sdms/make_sdms.jl
@@ -11,7 +11,7 @@ function main()
         sdms = make_sdms(s, occurrence_df; cluster=cluster)
     
         for (i,sdm) in enumerate(sdms)
-            SpeciesDistributionToolkit.save("$s_tmp$i.tif", sdm.probability)
+            SpeciesDistributionToolkit.save("$(s)_tmp$i.tif", sdm.probability)
             #ColoradoBumblebees.save(sdm)
         end
     end 
