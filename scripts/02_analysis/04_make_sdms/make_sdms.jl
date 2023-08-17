3f56c53360657a16b44af12c3ff4267b408925c7
michael <mdcatchen@gmail.com>
2023-08-17 11:21:41 -0400

    testing full save of sdms...

--- scripts/02_analysis/04_make_sdms/make_sdms.jl
+++ scripts/02_analysis/04_make_sdms/make_sdms.jl
@@ -11,7 +11,16 @@ function main()
         sdms = make_sdms(s, occurrence_df; cluster=cluster)
     
         for (i,sdm) in enumerate(sdms)
-            SpeciesDistributionToolkit.save("$(s)_tmp$i.tif", sdm.probability)
+            sdm_dir = sdmdir(sdm)
+            mkpath(sdm_dir)
+            @info sdm_dir
+            SpeciesDistributionToolkit.save(joinpath(sdm_dir, "prediction.tif"), sdm.probability)
+            SpeciesDistributionToolkit.save(joinpath(sdm_dir, "uncertainty.tif"), sdm.uncertainty)
+            json_string = JSON.json(sdm.fit_stats)
+            open(joinpath(sdm_dir, "fit.json"), "w") do f
+                JSON.print(f, json_string)
+            end
+            #SpeciesDistributionToolkit.save("$(s)_tmp$i.tif", sdm.probability)
             #ColoradoBumblebees.save(sdm)
         end
     end 
