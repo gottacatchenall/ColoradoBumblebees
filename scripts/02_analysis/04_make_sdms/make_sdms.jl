9c1ef31d5aaac2911be61b22bd1c4211aa109848
michael <mdcatchen@gmail.com>
2023-08-21 12:15:49 -0400

    i think its scratch files getting overwritten

--- scripts/02_analysis/04_make_sdms/make_sdms.jl
+++ scripts/02_analysis/04_make_sdms/make_sdms.jl
@@ -9,18 +9,24 @@ function main()
 
     job_id = parse(Int, ENV["SLURM_ARRAY_TASK_ID"])
     this_species = species[job_id]
-    @info "Job: $job_id, Species: $(this_species)"
 
-    mkpath("./failed")
+    mkpath("./tmp/$(this_species)")
+    cd("./tmp/$(this_species)")
+    @info "Job: $job_id, Species: $(this_species), WD: $(pwd())"
+
 
     sdms = make_sdms(this_species, occurrence_df; cluster=cluster, pa_buffer_distance=8)
     for (i,sdm) in enumerate(sdms)
         sdm_dir = sdmdir(sdm)
         mkpath(sdm_dir)
         
+
         # For some reason theres weird saving issues. 
         # Attempt 1: assume it is a GDAL <-> filesystem fuckup, and try writing
         # the same sdm a few times.
+
+        # They all write prediction local scratch files, maybe each job should
+        # cd into its own species specific dir?
         
         succeeded = false
         for attempt in 1:50
