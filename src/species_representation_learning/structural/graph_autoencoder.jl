2abec5beb7e5332ffeeb7b6ecf85f9406a76b54a
michael <mdcatchen@gmail.com>
2023-08-25 13:17:25 -0400

    graph ae needs to be kwdef

--- src/species_representation_learning/structural/graph_autoencoder.jl
+++ src/species_representation_learning/structural/graph_autoencoder.jl
@@ -2,3 +2,6 @@ Base.@kwdef struct GraphAutoencoder{S} <: Structural
     num_input_features = 0. 
     embedding_dim = 0.
 end
+
+outdim(gae::GraphAutoencoder{S}) where S = gae.embedding_dim
+outdim(gae::GraphAutoencoder{S}, ::Union{Type{Bee},Type{Plant}}) where S = outdim(gae)
