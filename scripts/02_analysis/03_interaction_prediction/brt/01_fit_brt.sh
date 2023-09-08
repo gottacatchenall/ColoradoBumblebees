dce14ae83455e08359f3c1377d84edfb310e7692
michael <mdcatchen@gmail.com>
2023-09-08 14:13:05 -0400

    longer runtime for brt

--- scripts/02_analysis/03_interaction_prediction/brt/01_fit_brt.sh
+++ scripts/02_analysis/03_interaction_prediction/brt/01_fit_brt.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks=1               
 #SBATCH --cpus-per-task=1        
 #SBATCH --mem-per-cpu=16G      
-#SBATCH --time=01:00:00         
+#SBATCH --time=02:00:00         
 #SBATCH --array=1-31
 
 module load cuda
