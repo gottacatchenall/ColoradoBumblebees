c47587a66945c4a404d2c4709aa1ba21256c2c37
michael <mdcatchen@gmail.com>
2023-09-18 10:57:04 -0400

    foo

---
+++ scripts/03_visualization/12_predicted_metaweb.jl
@@ -0,0 +1,71 @@
+# List of figures:
+
+# Main text:
+# ----------------------------------------------------------------------
+# - F007_predicted_metaweb.png
+
+using DrWatson
+using CairoMakie
+@quickactivate :ColoradoBumblebees
+
+data = load_data()
+bee_species, plants_species = bees(data), plants(data)
+bee_species, plants_species = bee_species[sortperm([b.name for b in bee_species])], plants_species[sortperm([p.name for p in plants_species])]
+
+binary_prediction, probability_prediction, empirical = ColoradoBumblebees.get_metaweb(BEST_FIT_DIR)
+
+M = BipartiteNetwork( Matrix{Bool}(any.(binary_prediction .∪ empirical)), [string(b.name) for b in bee_species], [string(p.name) for p in plants_species],)
+
+# Make nested 
+row_perm = sortperm(sum(eachrow(adjacency(M))))
+col_perm = sortperm(sum(eachcol(adjacency(M))))
+
+
+E = empirical[col_perm, row_perm]
+P = binary_prediction[col_perm, row_perm]
+
+f = Figure()
+ax = Axis(f[1,1])
+for i in findall(E)
+    scatter!(ax, [i[1]], [i[2]], color=:blue)
+end
+for i in findall(P)
+    scatter!(ax,  [i[1]], [i[2]], color=:red)
+end
+f
+
+
+bee_label = M.T[col_perm]
+plant_label = M.B[row_perm]
+
+mat = zeros(Int64,size(E))
+
+mat[findall(!iszero, P)] .= 2
+mat[findall(!iszero, E)] .= 1
+
+
+f = Figure(; resolution=(2900, 550))
+ax = Axis(
+    f[1, 1];
+    xticks=(1:(length(plant_label) .+ 0.5), plant_label),
+    xticklabelrotation=π / 2,
+    yticks=(1:length(bee_label), bee_label),
+    xminorgridvisible=true,
+    xminorgridcolor=:darkgrey,
+    yminorgridcolor=:darkgrey,
+    xminorticks=1:(length(plant_label) .+ 0.5),
+    yminorticks=1:(length(bee_label) .+ 0.5),
+)
+limits!(ax, 0.5, length(plant_label) + 0.5, 0.5, length(bee_label) + 0.5)
+
+getcol(val) = [:lightgrey, :deepskyblue2, :teal, :red][val + 1]
+
+hm = heatmap!(ax, zeros(size(mat))'; colormap=[:grey95])
+for (i, b) in enumerate(bee_label), (j, p) in enumerate(plant_label)
+    mat[i, j] > 0 &&
+        scatter!(ax, [j], [i]; marker=:rect, color=getcol(mat[i, j]), markersize=20)
+end
+
+
+
+f
