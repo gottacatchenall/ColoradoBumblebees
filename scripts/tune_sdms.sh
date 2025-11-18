ab2b093110d01fd513355dcb1852aee6323a3b9e
Michael D. Catchen <mdcatchen@gmail.com>
2025-11-18 10:47:54 -0500

    tuning run

--- scripts/tune_sdms.sh
+++ scripts/tune_sdms.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=1        
 #SBATCH --mem-per-cpu=16G 
 #SBATCH --array=1-180 
-#SBATCH --time=3:00:00         
+#SBATCH --time=12:00:00         
 
 export JULIA_DEPOT_PATH="/project/def-tpoisot/mcatchen/JuliaEnvironments/ColoradoBees"
 
